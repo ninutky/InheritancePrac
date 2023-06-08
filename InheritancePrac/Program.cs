3dbe5c978ee62cfad0398c19cea67581f716def4
Nagyeong Park <zzum0701@naver.com>
2023-06-08 14:57:52 +0900

    부모 자식 생성자 실습

--- InheritancePrac/Program.cs
+++ InheritancePrac/Program.cs
@@ -9,8 +9,28 @@ namespace InheritancePrac
 {
     internal class Program
     {
+        class Parent
+        {
+            public Parent() { Console.WriteLine("부모 생성자"); }
+            public Parent(int param) { Console.WriteLine("Parent(int param)"); }
+            public Parent(string param) { Console.WriteLine("parent(string param)"); }
+        }
+
+        class Child : Parent 
+        {   public Child() : base(10)
+            {
+                Console.WriteLine("Child() : base(10)");
+            }
+            public Child(string input) : base(input)
+            {
+                Console.WriteLine("Child(string input) : base(input)");
+            }
+        }
         static void Main(string[] args)
         {
+            Child childA = new Child();
+            Child childB = new Child("string");
+
             Dog d = new Dog();
             d.Public();
             // d.Protected();
